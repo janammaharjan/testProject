6331adb82adef8f35ccb285ac224c3775703e093
janammaharjan <janammaharjan@gmail.com>
2015-10-27 15:59:59 +0545

    added mapKit

--- testProject/ViewController.swift
+++ testProject/ViewController.swift
@@ -7,18 +7,41 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var locationInfo: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //HELLO BRO
+        var location = CLLocationCoordinate2DMake(8.8583736, 2.2922926)
+          var location2 = CLLocationCoordinate2DMake(8.8673736, 2.2932926)
+        var span = MKCoordinateSpanMake(1, 1)
+        var region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
         
-        //WORKING FROM DESKTOP
- //Working from Documents NOW
+        var annotation2 = MKPointAnnotation()
+        var annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation2.coordinate = location2
+        annotation2.title = "The NEXT"
+        annotation.title = "The Eiffle Tower Bro"
+        annotation.subtitle = "Janam Maharjan"
+        
+        mapView.addAnnotation(annotation)
+        mapView.addAnnotation(annotation2)
+  
             }
 
+    @IBAction func updateLocation(sender: AnyObject) {
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
